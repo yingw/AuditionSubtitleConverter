558a48726d1c7af738beb1ccc92c2b70c7ffa5a0
Yin Guo Wei <yinguowei@gmail.com>
2024-03-29 23:48:07 +0800

    Main program first commit.

---
+++ AuditionSubtitleConverter.py
@@ -0,0 +1,87 @@
+"""
+srt 字幕文件转成 Audition marker 的 csv 格式，另一个网上的版本 https://github.com/Terisback/subkers 有点bug
+"""
+
+import pysrt
+import csv
+from datetime import datetime
+from pysrt.srttime import SubRipTime
+from pysrt.srtitem import SubRipItem
+import os
+import sys
+import argparse
+
+def check_file_exists(targetfile):
+    """检查文件是否存在，并询问是否覆盖"""
+    if os.path.exists(targetfile):
+        print(f"File '{targetfile}' exists.")
+        if input("Overwrite? (y/n): ").lower() != "y":
+            print("Operation canceled.")
+            sys.exit(1)
+
+
+def convert_srt_to_marker(filename):
+    """将SRT字幕文件转换为Audition marker的CSV格式"""
+    if not filename.lower().endswith(".srt"):
+        print("Error: Only support *.srt file. Please choose another file.")
+        sys.exit(1)
+
+    if not os.path.exists(filename):
+        print(f"Error: File '{filename}' not found.")
+        sys.exit(1)
+
+    try:
+        subs = pysrt.open(filename)
+    except pysrt.Error.InvalidSubtitleError as e:
+        print(f"Error: Invalid subtitle file '{filename}': {e}")
+        sys.exit(1)
+
+    targetfile = os.path.splitext(filename)[0] + ".csv"
+    check_file_exists(targetfile)
+
+    field_names = ["Name", "Start", "Duration", "Time Format", "Type", "Description"]
+
+    with open(targetfile, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile, delimiter="\t")
+        writer.writerow(field_names)
+        writer.writerow([])
+        for s in subs:
+            if isinstance(s, SubRipItem):
+                print(s)
+                name = s.text.replace("\n", " ")
+                duration = subtime_to_string(s.end - s.start)
+                start_time = subtime_to_string(s.start)
+                writer.writerow([name, start_time, duration, "decimal", "Cue", ""])
+    print("Coversion completed.")
+
+
+def subtime_to_string(subtime: SubRipTime):
+    """将SubRipTime对象转换为时间字符串"""
+    try:
+        time_obj = datetime.strptime(str(subtime), "%H:%M:%S,%f")
+    except ValueError as e:
+        print(f"Error: Unable to parse time string '{subtime}': {e}")
+        return ""
+    hours = time_obj.hour
+    minutes = time_obj.minute
+    seconds = time_obj.second
+    milliseconds = time_obj.microsecond // 1000
+    return (
+        f"{minutes}:{seconds}.{milliseconds}"
+        if hours == 0
+        else f"{hours}:{minutes}:{seconds}.{milliseconds}"
+    )
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert SRT subtitle file to Adobe Audition marker CSV file.\n" \
+                                     "Usage: python AuditionSubtitleConverter.py <filename>")
+    parser.add_argument("filename", nargs="?", help="Input SRT subtitle file")
+    args = parser.parse_args()
+
+    if not args.filename:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    convert_srt_to_marker(args.filename)
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# AuditionSubtitleConverter
-Convert SRT subtitle file to Adobe Audition marker CSV file.
+# Audition Subtitle Converter
+
+This Python script converts SRT subtitle files to Adobe Audition marker CSV files. The original version found on [GitHub](https://github.com/Terisback/subkers) had some bugs, so this script aims to provide a more reliable solution.
+
+Audition markers file is a csv which looks like:
+
+```txt
+Name	Start	Duration	Time Format	Type	Description
+
+Some Text	2:1.540	0:6.110	decimal	Cue	
+Some Text2	2:7.690	0:4.340	decimal	Cue	
+```
+
+The values in the 'Start' and 'Duration' fields, '2:1.540' and '0:6.110' respectively, are not in standard format as expected (in SRT format: '00:02:01,540' --> '00:02:07,650'). This discrepancy needs to be addressed.
+
+Another online version <https://github.com/Terisback/subkers>
+
+## Requirements
+
+- Python 3.x
+- pysrt library
+
+You can install the required library using pip:
+
+```bash
+pip install pysrt
+```
+
+## Usage
+
+Run the script with the following command:
+
+```py
+python srt_to_marker.py <filename.srt>
+```
+
+Replace `<filename>` with the path to your SRT subtitle file.
+
+TODO:
+[ ] Convert csv to srt reversed.
